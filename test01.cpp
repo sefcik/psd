26e5dd6986a30b63fe7a48a1809a567dac6d7ce1
Michal Sefcik <michal.sefcik@cern.ch>
2016-05-20 11:01:17 +0200

    added function calculate

--- test01.cpp
+++ test01.cpp
@@ -6,9 +6,23 @@
 //
 //
 
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void calculate(long int n) {
+    double average=0;
+    for(long int i=0; i<n; i++) {
+        double random = (double)rand() / RAND_MAX * 2*M_PI;
+        average += sin(random) + 0.5;
+    }
+    average /= n;
+    printf("Average = %f\n",average);
+}
 
 int main() {
-    printf("Hello\n");
+    srand(time(NULL));
+    calculate(1e9);
     return 0;
 }
